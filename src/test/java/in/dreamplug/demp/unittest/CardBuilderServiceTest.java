b661dc9333ad1fd45f30f05ac670e8bf411a449c
Gaurav Kumar <=>
2022-04-14 16:41:27 +0530

    add CardBuilderService tests

--- src/test/java/in/dreamplug/demp/unittest/CardBuilderServiceTest.java
+++ src/test/java/in/dreamplug/demp/unittest/CardBuilderServiceTest.java
@@ -2,8 +2,16 @@ package in.dreamplug.demp.unittest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import java.util.concurrent.ThreadLocalRandom;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import in.dreamplug.demo.unittest.domain.Card;
 import in.dreamplug.demo.unittest.domain.CardType;
 import in.dreamplug.demo.unittest.service.CardBuilderService;
@@ -12,22 +20,29 @@ import in.dreamplug.demo.unittest.service.CardBuilderService;
  * @author gauravkumar
  * @since 13/04/22
  */
+@ExtendWith (MockitoExtension.class)
 public class CardBuilderServiceTest {
-    CardBuilderService cardBuilderService = new CardBuilderService(ThreadLocalRandom.current());
+    @InjectMocks
+    CardBuilderService cardBuilderService;
+
+    @Mock
+    ThreadLocalRandom threadLocalRandom;
 
     @Test
     public void testBuildCard() {
         CardType cardType = CardType.black;
+        /*
+         * can use any() when argument is dynamic
+         * */
+        when(threadLocalRandom.nextInt(eq(100), eq(999))).thenReturn(255);
         Card card = cardBuilderService.buildCard(CardType.black);
-        //notNull assertions
-        assertNotNull(card.getCvv());
+        assertEquals("255", card.getCvv());
+        verify(threadLocalRandom, times(1)).nextInt(eq(100), eq(999));
+        //TODO add mocks for number
         assertNotNull(card.getNumber());
         assertEquals(cardType.getBrand(), card.getBrand());
         assertEquals(cardType, card.getCardType());
         assertEquals(cardType.getJoiningFee(), card.getJoiningFee());
         assertEquals(cardType.getRenewalFee(), card.getRenewalFee());
-        /*
-         * How can we validate values assigned to cvv, number
-         * */
     }
 }
