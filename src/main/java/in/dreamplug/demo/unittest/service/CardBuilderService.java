fb8e82e1ea0e0a72816a6e76f89e0eec5de3ab54
Gaurav Kumar <=>
2022-04-15 12:21:09 +0530

    add bug

--- src/main/java/in/dreamplug/demo/unittest/service/CardBuilderService.java
+++ src/main/java/in/dreamplug/demo/unittest/service/CardBuilderService.java
@@ -16,6 +16,7 @@ public class CardBuilderService {
     public Card buildCard(CardType cardType) {
         Long cardNumber = r.nextLong(1_000_000_000_000_000l, 9_999_999_999_999_999l);
         Integer cvv = r.nextInt(100, 999);
+        cardNumber = r.nextLong(1_000_000_000_000_000l, 9_999_999_999_999_999l);
         return Card.builder().cardType(cardType).brand(cardType.getBrand()).number(cardNumber.toString()).cvv(cvv.toString())
                    .joiningFee(cardType.getJoiningFee()).renewalFee(cardType.getRenewalFee()).build();
     }
